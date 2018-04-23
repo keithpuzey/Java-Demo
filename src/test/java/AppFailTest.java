b978fe77c3c18fdb704796b7e1a543edc53ce753
keithpuzey <puzke01@ca.com>
2018-04-23 15:19:05 +0100

    Java Demo

---
+++ src/test/java/AppFailTest.java
@@ -0,0 +1,10 @@
+/*Creating a failure test case*/
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppFailTest {
+    @Test public void testAppHasAGreeting() {
+        App classUnderTest = new App();
+        assertNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
